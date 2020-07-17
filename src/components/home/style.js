fb32a0e050008908dc3ebaeb5c400ed2a4e6e815
williamChang80 <william_chang80@rocketmail.com>
2020-07-17 16:51:05 +0700

    Added car don home screen

--- src/components/home/style.js
+++ src/components/home/style.js
@@ -1,3 +1,61 @@
 import styled from "styled-components";
 
-export const HomeContainer = styled.div``;
+export const HomeContainer = styled.div`
+  .item-container {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    margin: 0 5%;
+  }
+  .card {
+    margin-top: -2.5px !important;
+    border: 3px solid black;
+  }
+
+  .action-delete {
+    color: red;
+  }
+  .action-delete:hover {
+    opacity: 0.8;
+    cursor: pointer;
+  }
+  .action-edit {
+    color: orange;
+  }
+  .action-edit:hover {
+    opacity: 0.8;
+    cursor: pointer;
+  }
+  .btn-add {
+    background-color: green;
+  }
+  .action-buy {
+    font-size: 1.7rem;
+    font-family: "Montserrat";
+    width: fit-content;
+    padding: 0 1rem;
+    border-radius: 0.5rem 0.5rem 0 0;
+    color: #08cf90;
+    background-color: white;
+    margin: 0 2rem;
+    font-weight: bold;
+    border: 3px solid black;
+  }
+  .action-buy:hover {
+    color: #07875f;
+    cursor: pointer;
+  }
+  .buy {
+    color: white;
+  }
+  .page-container {
+    margin: 2rem;
+  }
+  .page-title {
+    color: white;
+    font-family: "Montserrat";
+    text-align: center;
+    font-weight: bold;
+    font-size: 3rem;
+  }
+`;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,108 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { toast } from "react-toastify";
+import axios from "axios";
 
 import { HomeContainer } from "components/home/style";
 import Navbar from "components/navbar";
+import { useState } from "react";
+import Card from "components/card";
+import Spinner from "components/spinner";
+import {
+  getAllItemsUrl,
+  getItemsByUserIdUrl,
+  getItemsByCategoryIdUrl,
+  deleteItemUrl
+} from "config/Url";
+import App from "config/App";
+const { appUrl } = App;
 
 const HomePage = () => {
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      name: "ASUS",
+      description: "This is ASUS",
+      category: {
+        id: 1,
+        createdAt: "2020-07-17T05:21:42.456+00:00",
+        updatedAt: "2020-07-17T05:21:42.456+00:00",
+        name: null
+      },
+      price: 500000,
+      user: {
+        id: 1,
+        username: "1236"
+      }
+    }
+  ]);
+  const imageUrl = `${appUrl}/Images/Item.jpg`;
+  useEffect(() => {
+    axios
+      .get(getAllItemsUrl, {
+        headers: {
+          Authorization: `Bearer ${window.localStorage.getItem("accessToken")}`
+        }
+      })
+      .then((res) => setItems(res.data.data))
+      .catch((e) => toast.error(e.message));
+  });
+
+  const renderItems = () => {
+    return !!items.length ? renderCards() : <Spinner />;
+  };
+
+  const buyItems = (itemId, userId) => {
+    userId === window.localStorage.getItem("userId")
+      ? toast.error("Cannot buy your own item")
+      : axios
+          .delete(deleteItemUrl(itemId), {
+            headers: {
+              Authorization: `Bearer ${window.localStorage.getItem(
+                "accessToken"
+              )}`
+            }
+          })
+          .then(
+            (res) =>
+              res.data.code === 200 && toast.success("Success Bought Item")
+          )
+          .catch((e) => toast.error(e.message));
+  };
+
+  const renderBuyButton = (itemId, userId) => {
+    return (
+      <div className="action-buy" onClick={() => buyItems(itemId, userId)}>
+        <div className="fa fa-money" /> Buy Item
+      </div>
+    );
+  };
+
+  const renderCards = () => {
+    return (
+      <div className="item-container">
+        {items.map((item) => (
+          <div className="buy" key={item.id}>
+            {renderBuyButton(item.id, item.user.id)}
+            <Card
+              title={item.name}
+              image={imageUrl}
+              description={item.description}
+              price={item.price}
+              owner={item.user.username}
+              category={item.category.name}
+            />
+          </div>
+        ))}
+      </div>
+    );
+  };
   return (
     <HomeContainer>
       <Navbar />
+      <div className="page-container">
+        <div className="page-title">Home</div>
+        {renderItems()}
+      </div>
     </HomeContainer>
   );
 };
